62128d627a4cfd72ea4b098fb0aef16bd93086e7
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-06 16:00:21 -0300

    add file env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 
 @Module({
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true
+  })],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
