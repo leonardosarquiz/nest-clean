9f7e5a075d5eabd6e3ce8bf2216f80f39684aaf8
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-07 15:58:07 -0300

    add authenticate

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { Env } from 'src/env'
+
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService], global: true, useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+        return {
+
+          privateKey,
+          publicKey
+        }
+      },
+    })
+  ]
+})
+export class AuthModule { }
